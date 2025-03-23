936a367adfe03c876b933f5944a2cc4094f2c018
Your Name <you@example.com>
2025-03-23 11:15:27 +0700

    sửa lỗi 4

--- view_logs.php
+++ view_logs.php
@@ -1,5 +1,5 @@
 <?php
-$filename = "logins.txt";
+$filename = "data/logins.txt"; // Đường dẫn đúng tới file
 
 if (file_exists($filename)) {
     echo "<pre>" . htmlspecialchars(file_get_contents($filename)) . "</pre>";
